411cdbaeb6f4995fc9a12517c45d79bd299f4d3a
Shubham.Jain <shubham.jain@sapiens.com>
2021-09-02 12:12:34 +0530

    DECISION-26288_accommodate_review_comments

--- libs/angular-breadcrumb/src/breadcrumb/directive/highlight-current-opened-item.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HighlightCurrentOpenedItemDirective } from './highlight-current-opened-item.directive';
-
-describe('HighlightCurrentOpenedItemDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HighlightCurrentOpenedItemDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- libs/angular-breadcrumb/src/common/model/dropdown.model.ts
+++ libs/angular-breadcrumb/src/common/model/dropdown.model.ts
@@ -1,10 +1,8 @@
 import {Observable} from "rxjs";
 import {BreadcrumbDropDownItem} from "./dropdown-item.model";
-import {ActivatedRoute, ActivatedRouteSnapshot} from "@angular/router";
 
 export interface BreadcrumbDropDown {
     popupTitle?: string;
     items?: BreadcrumbDropDownItem[] |  Observable<BreadcrumbDropDownItem[]>;
     getItems?: () => BreadcrumbDropDownItem[] |  Observable<BreadcrumbDropDownItem[]>;
-    activatedRoute?:ActivatedRouteSnapshot;
 }
